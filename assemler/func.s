cb3b401ff86be87a62109a99cb545b5bb9432ff9
alexeyKos02 <113934805+alexeyKos02@users.noreply.github.com>
2022-11-24 16:10:33 +0600

    Update func.s

--- assemler/func.s
+++ assemler/func.s
@@ -28,33 +28,33 @@ diff:
 	mov	rdi, r9
 	mov	rdi, rax
 	mov	QWORD PTR -32[rbp], rsi
-	mov	QWORD PTR -24[rbp], rdi
-	mov	QWORD PTR -48[rbp], rdx
+	mov	QWORD PTR -24[rbp], rdi					# в стек end
+	mov	QWORD PTR -48[rbp], rdx					# в стек кидаем st
 	mov	QWORD PTR -40[rbp], rcx
 	mov	rax, QWORD PTR -32[rbp]
 	pxor	xmm1, xmm1
-	cvtsi2sd	xmm1, rax
-	movsd	xmm0, QWORD PTR .LC0[rip]
-	mulsd	xmm1, xmm0
-	mov	rax, QWORD PTR -24[rbp]
+	cvtsi2sd	xmm1, rax					# преобразует и кладет значение st.tv_sec в xmm1
+	movsd	xmm0, QWORD PTR .LC0[rip]				# в xmm0 кладет значение 10е8
+	mulsd	xmm1, xmm0						# st.tv_sec *10e8
+	mov	rax, QWORD PTR -24[rbp]					# в rax кладут st.tv_nsec
 	pxor	xmm0, xmm0
-	cvtsi2sd	xmm0, rax
-	addsd	xmm0, xmm1
-	cvttsd2si	rax, xmm0
-	mov	QWORD PTR -8[rbp], rax				# tot1 = st.tv_sec *10e8 +st.tv_nsec;
-	mov	rax, QWORD PTR -48[rbp]
+	cvtsi2sd	xmm0, rax					# в xmm0 кладем st.tv_nsec
+	addsd	xmm0, xmm1						# складываем две части
+	cvttsd2si	rax, xmm0					# в rax кладем значение суммы
+	mov	QWORD PTR -8[rbp], rax
+	mov	rax, QWORD PTR -48[rbp]					# в rax кладем end.tv_sec
 	pxor	xmm1, xmm1
-	cvtsi2sd	xmm1, rax
-	movsd	xmm0, QWORD PTR .LC0[rip]
+	cvtsi2sd	xmm1, rax					# в xmm1 кладем end.tv_sec
+	movsd	xmm0, QWORD PTR .LC0[rip]				# в xmm0 кладет значение 10е8
 	mulsd	xmm1, xmm0
-	mov	rax, QWORD PTR -40[rbp]
+	mov	rax, QWORD PTR -40[rbp]					# end.tv_sec *10e8
 	pxor	xmm0, xmm0
-	cvtsi2sd	xmm0, rax
-	addsd	xmm0, xmm1
-	cvttsd2si	rax, xmm0
-	mov	QWORD PTR -16[rbp], rax				# tot2 = end.tv_sec *10e8 +end.tv_nsec;
+	cvtsi2sd	xmm0, rax					# в xmm0 кладем end.tv_nsec
+	addsd	xmm0, xmm1						# складываем обе части
+	cvttsd2si	rax, xmm0					# преобразуем и кладем в rax сумму
+	mov	QWORD PTR -16[rbp], rax					# в стек кидаем значение второй суммы
 	mov	rax, QWORD PTR -8[rbp]
-	sub	rax, QWORD PTR -16[rbp]
+	sub	rax, QWORD PTR -16[rbp]					# tot1-tot2
 	lea	rdx, .LC1[rip]
 	mov	rsi, rax
 	lea	rax, .LC2[rip]
@@ -80,26 +80,26 @@ Alg:
 	divsd	xmm0, xmm1
 	movsd	QWORD PTR -16[rbp], xmm0
 	mov	eax, DWORD PTR degree[rip]
-	sub	eax, 1
+	sub	eax, 1							# (degree - 1)
 	pxor	xmm0, xmm0
 	cvtsi2sd	xmm0, eax
-	mulsd	xmm0, QWORD PTR -8[rbp]
+	mulsd	xmm0, QWORD PTR -8[rbp]					# (degree - 1) * number
 	movsd	QWORD PTR -24[rbp], xmm0
 	movsd	xmm3, QWORD PTR A[rip]
 	movsd	QWORD PTR -32[rbp], xmm3
 	mov	eax, DWORD PTR degree[rip]
-	sub	eax, 1
+	sub	eax, 1							# degree - 1
 	pxor	xmm0, xmm0
 	cvtsi2sd	xmm0, eax
 	mov	rax, QWORD PTR -8[rbp]
 	movapd	xmm1, xmm0
 	movq	xmm0, rax
-	call	pow@PLT
+	call	pow@PLT							# pow(number, degree - 1)
 	movsd	xmm1, QWORD PTR -32[rbp]
-	divsd	xmm1, xmm0
+	divsd	xmm1, xmm0						#  A / pow(number, degree - 1))
 	movsd	xmm0, QWORD PTR -24[rbp]
 	addsd	xmm0, xmm1
-	mulsd	xmm0, QWORD PTR -16[rbp]
+	mulsd	xmm0, QWORD PTR -16[rbp]				# degree * ((degree - 1) * number + A / pow(number, degree - 1));
 	movq	rax, xmm0
 	movq	xmm0, rax
 	leave
@@ -118,7 +118,7 @@ Sqrt:
 	lea	rax, -48[rbp]
 	mov	rsi, rax
 	mov	edi, 1
-	call	clock_gettime@PLT
+	call	clock_gettime@PLT				# clock_gettime(CLOCK_MONOTONIC, &st);
 	movsd	xmm0, QWORD PTR .LC4[rip]
 	mov	rax, QWORD PTR -72[rbp]
 	movapd	xmm1, xmm0
@@ -139,27 +139,27 @@ Sqrt:
 .L6:
 	mov	rax, QWORD PTR -8[rbp]
 	movq	xmm0, rax
-	call	Alg
+	call	Alg						# Alg(alg)
 	movq	rax, xmm0
-	mov	QWORD PTR -8[rbp], rax
+	mov	QWORD PTR -8[rbp], rax				# alg = Alg(alg);
 .L5:
 	movsd	xmm0, QWORD PTR -8[rbp]
 	subsd	xmm0, QWORD PTR -16[rbp]
 	movq	xmm1, QWORD PTR .LC6[rip]
-	andpd	xmm0, xmm1
-	comisd	xmm0, QWORD PTR -24[rbp]
+	andpd	xmm0, xmm1					# fabs(alg - corr)
+	comisd	xmm0, QWORD PTR -24[rbp]			# fabs(alg - corr) > eps
 	ja	.L6
 	lea	rax, -64[rbp]
 	mov	rsi, rax
 	mov	edi, 1
-	call	clock_gettime@PLT
+	call	clock_gettime@PLT				# clock_gettime(CLOCK_MONOTONIC, &end);
 	mov	rax, QWORD PTR -48[rbp]
 	mov	rdx, QWORD PTR -40[rbp]
 	mov	rdi, QWORD PTR -64[rbp]
 	mov	rsi, QWORD PTR -56[rbp]
 	mov	rcx, rdx
 	mov	rdx, rax
-	call	diff
+	call	diff						# diff(end,st);
 	movsd	xmm0, QWORD PTR -8[rbp]
 	movq	rax, xmm0
 	movq	xmm0, rax
